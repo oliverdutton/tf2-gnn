b19ecd4556dbef2b8fe04cbcf439c5e0c25a3825
oliverdutton <44170519+oliverdutton@users.noreply.github.com>
2020-07-09 11:14:31 +0200

    Exposed train from command line utils to python import

--- tf2_gnn/__init__.py
+++ tf2_gnn/__init__.py
@@ -1,3 +1,4 @@
 from .layers import GNNInput, GNN, get_known_message_passing_classes
 from .models import GraphTaskModel, NodeMulticlassTask, GraphRegressionTask, GraphBinaryClassificationTask
 from .data import DataFold, GraphSample, GraphBatchTFDataDescription, GraphDataset, JsonLGraphDataset
+from .cli_utils.training_utils import train
