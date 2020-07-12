d8f852ad3481d77dc304d9b057a0135369612c6b
oliverdutton <44170519+oliverdutton@users.noreply.github.com>
2020-07-12 16:11:27 +0200

    Update __init__.py

--- tf2_gnn/utils/__init__.py
+++ tf2_gnn/utils/__init__.py
@@ -2,3 +2,4 @@ from .activation import gelu
 from .constants import SMALL_NUMBER
 from .gather_dense_gradient import gather_dense_gradient
 from .param_helpers import get_activation_function, get_aggregation_function
+from .metrics import micro_f1
