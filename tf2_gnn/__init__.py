a20242e1008f450bb02d91215616f4d343385c51
oliverdutton <44170519+oliverdutton@users.noreply.github.com>
2020-07-12 16:08:11 +0200

    command line utils exposed

--- tf2_gnn/__init__.py
+++ tf2_gnn/__init__.py
@@ -2,4 +2,5 @@ from .layers import GNNInput, GNN, get_known_message_passing_classes
 from .models import GraphTaskModel, NodeMulticlassTask, GraphRegressionTask, GraphBinaryClassificationTask
 from .data import DataFold, GraphSample, GraphBatchTFDataDescription, GraphDataset, JsonLGraphDataset
 from .cli_utils.training_utils import train
-from .cli_utils.model_utils import *
+from .cli_utils.model_utils import load_weights_verbosely, save_model
+from .cli.test import test
