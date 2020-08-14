cda2d71d3b81ac1d22b6f32d59ced4b4705443d6
oliverdutton <44170519+oliverdutton@users.noreply.github.com>
2020-08-14 09:30:09 +0200

    task_metrics simplified using super() and results squashed to [1,3) range to help network

--- tf2_gnn/models/node_regression_task_pss.py
+++ tf2_gnn/models/node_regression_task_pss.py
@@ -4,9 +4,39 @@ import numpy as np
 import tensorflow as tf
 
 from tf2_gnn.data import GraphDataset
-from tf2_gnn.models import NodeRegressionTask
+from tf2_gnn.models import NodeRegressionTask, NodeMulticlassTask
 
 class PSSTask(NodeRegressionTask):
+    def __init__(self, params: Dict[str, Any], dataset: GraphDataset, name: str = None):
+        super().__init__(params, dataset=dataset, name=name)
+     
+    def compute_task_output(
+        self, batch_features, final_node_representations, training: bool
+    ):
+        (per_node_results,) = super().compute_task_output(batch_features, final_node_representations, training)
+        """ Output layer to scale for output range [1,3) to fit secondary structure"""
+        per_node_results = tf.sigmoid(per_node_results)*2+1
+        return (per_node_results,)
+
+    def compute_task_metrics(
+        self, batch_features, task_output, batch_labels
+    ) -> Dict[str, tf.Tensor]:
+        """ Unpack outputs """
+        (node_predictions,) = task_output
+        node_values = batch_labels["node_values"]
+
+        """ 
+        Extra section to mask missing node_values in experimental so they don't contribute
+            to losses
+        """
+        mask = np.not_equal(node_values,0)
+        node_values, node_predictions = node_values[mask], tf.boolean_mask(node_predictions, mask, axis=1)
+        """ Repack outputs """
+        task_output, batch_labels["node_values"] = (node_predictions,), node_values
+        return super().compute_task_output(batch_features, task_output, batch_labels)
+
+
+class PSSTask_Classes(NodeMulticlassTask):
     def __init__(self, params: Dict[str, Any], dataset: GraphDataset, name: str = None):
         super().__init__(params, dataset=dataset, name=name)
      
